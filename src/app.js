52941e39630a37c7567f98e7f0042c3f6ef386c4
lidicev <lidice.villarreal001@mymdc.net>
2023-03-16 00:15:35 +0000

    comment

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function generateRandomDomain() {
+  const domains = [".com", ".org", ".net", ".io", ".co", ".me"];
+  const words = ["apple", "banana", "orange", "grape", "kiwi", "pineapple"];
+  let domainName = "";
+
+  for (let i = 0; i < 3; i++) {
+    const randomIndex = Math.floor(Math.random() * words.length);
+    const word = words[randomIndex];
+    domainName += word;
+  }
+
+  const randomIndex = Math.floor(Math.random() * domains.length);
+  const domain = domains[randomIndex];
+  domainName += domain;
+
+  return domainName;
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let l = generateRandomDomain();
+  console.log(l);
+  document.querySelector("#the-domain").innerHTML = l;
 };
+
+console.log("Hello Rigo from the console!");
+document.querySelector("#btn").addEventListener("click", () => {
+  document.querySelector("#the-domain").innerHTML = generateRandomDomain();
+});
+
+const myDomain = generateRandomDomain();
+console.log(myDomain);
